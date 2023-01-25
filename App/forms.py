e0db8cc9cdbed5d4a168437a24295e97d5b3bc4c
ishhabhatia <ishabhatia22@gmail.com>
2023-01-25 15:01:53 +0530

    initial commit

---
+++ App/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Employee_model
+
+
+class Employee_form(forms.ModelForm):
+    class Meta:
+        model = Employee_model
+        fields = "__all__"
---
+++ App/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from .forms import Employee_form
+from .models import Employee_model
+
+
+# Create your views here.
+
+def savedemo(request):
+    if request.method == "POST":
+        data = Employee_form(request.POST)
+        if data.is_valid():
+            data.save()
+            return redirect("/")
+    else:
+        fm = Employee_form()
+        return render(request, 'show.html', {'form': fm})
+
+
+def displaydata(request):
+    data = Employee_model.objects.all()
+    return render(request, 'index.html', {'res': data})
+
+
+def edit(request, id):
+    data = Employee_model.objects.get(id=id)
+    return render(request, 'edit.html', {"res": data})
+
+
+def updatedata(request, id):
+    emp = Employee_model.objects.get(id=id)
+    data = Employee_form(request.POST, instance=emp)
+    if data.is_valid():
+        data.save()
+        return redirect("/index")
+
+
+def deletedata(request, id):
+    data = Employee_model.objects.get(id=id)
+    data.delete()
+    return redirect("/index")
